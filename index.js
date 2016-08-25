9a44095805ca07ee7af633d199bf9a51009f4646
teemopower <jimmykim925@gmail.com>
2016-08-25 00:58:45 -0700

    added headers

--- index.js
+++ index.js
@@ -17,6 +17,12 @@ var app = express();
 var summonerName;
 var summonerNameLive;
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
